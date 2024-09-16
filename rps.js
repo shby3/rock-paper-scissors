c9325a21abe193bbaf7bf7504f1de6b69f6530eb
shby3 <ashby.loshin@gmail.com>
2024-09-15 21:20:43 -0400

    Add function getHumanChoice
    The function getHumanChoice prompts a user for rock, paper, scissors,
    and continues to prompt them until they give a valid answer. The valid
    answer is returned in all lowercase.

--- rps.js
+++ rps.js
@@ -1,7 +1,14 @@
 choices = ['rock', 'paper', 'scissors']
-
 getRandomInt = max => Math.floor(Math.random() * max)
 
+// Function to return 'rock', 'paper', or 'scissors'
 getComputerChoice = () => choices[getRandomInt(3)]
 
-console.log(getComputerChoice())
+// Prompt the user for a valid choice and return it.
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, or scissors?")
+    while (!choices.includes(choice.toLowerCase())) {
+        choice = prompt("You can only choose rock, paper, or scissors.")
+    }
+    return choice
+}
